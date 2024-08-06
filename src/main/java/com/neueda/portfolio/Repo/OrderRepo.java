58c8de7333a42b9efa2d5c32ae5b98ccd8818f63
sneha_0723 <vsneha801@gmail.com>
2024-08-06 23:38:17 +0530

    bug fic

--- src/main/java/com/neueda/portfolio/Repo/OrderRepo.java
+++ src/main/java/com/neueda/portfolio/Repo/OrderRepo.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface OrderRepo extends JpaRepository<Orders, Long> {
-    public List<Orders> findBytickerSymbol(String tickerSymbol);git
+    public List<Orders> findBytickerSymbol(String tickerSymbol);
 }
