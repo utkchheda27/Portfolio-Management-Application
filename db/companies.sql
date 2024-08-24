d7606ee1ca0fbfd48295df438cccf4ed931f613e
Utkarsh Chheda <utkchheda27@gmail.com>
2024-08-24 17:17:18 +0530

    Added proj directories

---
+++ db/companies.sql
@@ -0,0 +1,12 @@
+INSERT INTO `stock` (`ticker_symbol`,`company_name`,`stock_price`,`week52high`,`week52low`,`average_volume`,`industry`,`market_exchange`)
+VALUES
+('AAPL','Apple Inc.',224.86,237.23,164.08,52351892,'Consumer Electronics','NMS'),
+('MSFT','Microsoft Corporation',419.27,468.35,309.45,19105081,'Software - Infrastructure','NMS'),
+('JPM','JPMorgan Chase & Co.',211.46,217.56,135.19,8887508,'Banks - Diversified','NYQ'),
+('TSLA','Tesla, Inc.',213.83,278.98,138.80,101785975,'Auto Manufacturers','NMS'),
+('T','AT&T Inc.',19.22,19.99,13.92,36307754,'Telecom Services','NYQ'),
+('VZ','Verizon Communications Inc.',40.27,43.42,30.14,16587002,'Telecom Services','NYQ'),
+('AMZN','Amazon.com, Inc.',175.46,201.20,118.35,40697195,'Internet Retail','NMS'),
+('F','Ford Motor Company',10.49,14.85,9.49,51658406,'Auto Manufacturers','NYQ'),
+('PFE','Pfizer Inc.',28.67,37.19,25.20,39113072,'Drug Manufacturers - General','NYQ'),
+('WMT','Walmart Inc.',73.29,74.44,49.85,16018348,'Discount Stores','NYQ')
---
+++ db/instrument.sql
@@ -0,0 +1,15 @@
+INSERT INTO `instrument` (`ticker_symbol`,`average_buy_price`,`company_name`,`volume`,`type`)
+VALUES
+('AAPL',655.12,'Apple Inc.',300,'stock'),
+('AMZN',818.33,'Amazon.com, Inc.',120,'stock'),
+('CORP1',92450.54,'Corporation A',2,'bond'),
+('JPM',508.68,'JPMorgan Chase & Co.',200,'stock'),
+('MSFT',2858.58,'Microsoft Corporation',250,'stock'),
+('MUNI1',4866.68,'Municipality A',1,'bond'),
+('PYPL',65.27,'PayPal',201,'stock'),
+('T',2760.56,'AT&T Inc.',300,'stock'),
+('TSLA',937.75,'Tesla, Inc.',100,'stock'),
+('US10Y',47074.99,'US Treasury A',2,'bond'),
+('US30Y',8470.39,'US Treasury B',1,'bond'),
+('VZ',1742.33,'Verizon Communications Inc.',350,'stock'),
+('WMT',69.27,'Walmart',130,'stock');
